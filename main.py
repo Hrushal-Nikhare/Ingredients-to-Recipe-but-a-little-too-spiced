d6e24255b1eea47aa7c258d400c45bb5446e6014
Hrushal Nikhare <hrushalnikhare@duck.com>
2022-10-09 22:00:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import subprocess
 
 inp = input("The Ingredients you have(for multiple separate them with a space): ").split()
 inp = ",+".join(inp)
-r = requests.get("https://api.spoonacular.com/recipes/findByIngredients",params={"ingredients":inp,"apiKey":"f670bda27e594092a108f95f3dcadee7","number":1})
+r = requests.get("https://api.spoonacular.com/recipes/findByIngredients",params={"ingredients":inp,"apiKey":"Your spoonacular API Kay","number":1})
 data = json.loads(r.text)
 print()
 url = data[0]['image']
